b8ec9df08002a685d0ead5d7f86e693681357de9
GDR! <gdr@go2.pl>
2013-07-02 15:27:35 +0200

    Changes to pass validation with fewer warnings

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zip -r ../paranoia.xpi * -x .git\* -x modules/stdlib/.git\*
+zip -r ../paranoia.xpi * -x .git\* -x modules/stdlib/.git\* -x website/\* -x \*.sh -x .\* -x \*/.directory -x modules\*
